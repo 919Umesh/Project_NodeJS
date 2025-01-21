fb816ae18299e47e40145537b9f21fc73b801599
Umesh <globaltechumesh11@gmail.com>
2025-01-21 13:41:44 +0545

    Login

--- middlewares/token.js
+++ middlewares/token.js
@@ -5,17 +5,23 @@ function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
 
     if (!authHeader) {
-        return res.status(401).json({ message: 'Authorization header is missing' });
+        return res.status(401).json({
+             status:401,
+            message: 'Authorization header is missing' });
     }
 
-    const token = authHeader.split(' ')[1]; // Format: "Bearer <token>"
+    const token = authHeader.split(' ')[1]; 
     if (!token) {
-        return res.status(401).json({ message: 'Token is missing' });
+        return res.status(401).json({ 
+            status:401,
+            message: 'Token is missing' });
     }
 
     jwt.verify(token, SECRET_KEY, (err, user) => {
         if (err) {
-            return res.status(403).json({ message: 'Invalid or expired token' });
+            return res.status(403).json({
+                 status: 403, 
+                 message: 'Invalid or expired token' });
         }
 
         req.user = user; 
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { handleCreateUser,handleGetAllUsers,handleLoginUser} = require('../controllers/users');
-const { authenticateToken } = require('../middlewares/app');
+const { authenticateToken } = require('../middlewares/token');
 
 router.post('/createUser',authenticateToken, handleCreateUser); 
 router.get('/getUsers',authenticateToken, handleGetAllUsers);
-router.post('/loginUser',authenticateToken, handleLoginUser);
+router.post('/loginUser', handleLoginUser);
  
 module.exports = router;
