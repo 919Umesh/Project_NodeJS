2dcf68fa5d550cc23deff0ce44339515fb6bf359
Umesh <globaltechumesh11@gmail.com>
2025-01-23 12:21:50 +0545

    Token

--- routes/location.js
+++ routes/location.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { handleCreateLocation, handleGetAllLocations, handleDeleteLocation } = require('../controllers/location');
 const { authenticateToken } = require('../middlewares/token');
 
-router.post('/createLocation', handleCreateLocation);
-router.get('/getLocations',  handleGetAllLocations);
-router.delete('/deleteLocation/:id', handleDeleteLocation);
+router.post('/createLocation',authenticateToken, handleCreateLocation);
+router.get('/getLocations', authenticateToken, handleGetAllLocations);
+router.delete('/deleteLocation/:id', authenticateToken,handleDeleteLocation);
 
 module.exports = router;
